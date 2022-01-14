0a7c559981b862c40ce3ee1f305b0dc8a53be016
4nth0ny1 <acatullo4@gmail.com>
2022-01-14 13:05:11 -0600

    add push function

--- stack.js
+++ stack.js
@@ -3,4 +3,10 @@ class Stack {
         this.storage = storage;
         this.count = count;
     }
+
+    // adds elemeent to the end/top of the stack 
+    push(value) {
+        this.storage[this.count] = value;
+        this.count++;
+    }
 }
